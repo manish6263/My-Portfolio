66a7ab5420605024a3c1e2ad458de9c974bf58d5
Manish <mp626329@gmail.com>
2022-06-21 22:08:57 +0530

    more updation in react server

--- src/routes/about.js
+++ src/routes/about.js
@@ -6,7 +6,7 @@ router.get(
     '/',
     async (req, res) => {
         try {
-            res.render('../../client/src/components/About.js');
+            res.redirect('/about');
         } catch (error) {
             console.log(error);
             res.status(500).send({ result: 'Internal server error', isSuccessfull: false });
