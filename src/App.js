de230be8139435b4a626ca4423fe40e2b2bdb3c7
Manish <mp626329@gmail.com>
2022-06-22 08:19:48 +0530

    another comment

--- src/App.js
+++ src/App.js
@@ -28,6 +28,9 @@ app.use('/about', require('./routes/about'));
 //for heroku deployment.......
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+  })
 }
 
 //Listening the express app.....
